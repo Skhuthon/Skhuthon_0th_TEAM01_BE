9c435e86e72ed464543b2c94726745cbb79941ee
dongkyunKim <kyun010713@gmail.com>
2024-06-26 05:46:46 +0900

    feat: CustomOAuth2UserService 추가 - OAuth2 공급자로부터 사용자 정보를 가져와서 UserEntity를 생성하거나 업데이트합니다.

---
+++ src/main/java/com/skhuthon/caffeinebalance/auth/service/CustomOAuth2UserService.java
@@ -0,0 +1,72 @@
+package com.skhuthon.caffeinebalance.auth.service;
+
+import com.skhuthon.caffeinebalance.auth.dto.CustomOAuth2User;
+import com.skhuthon.caffeinebalance.auth.dto.GoogleResponse;
+import com.skhuthon.caffeinebalance.auth.dto.NaverResponse;
+import com.skhuthon.caffeinebalance.auth.dto.OAuth2Response;
+import com.skhuthon.caffeinebalance.user.domain.SocialType;
+import com.skhuthon.caffeinebalance.user.domain.User;
+import com.skhuthon.caffeinebalance.user.dto.request.UserCreateRequestDTO;
+import com.skhuthon.caffeinebalance.user.dto.response.UserResponseDTO;
+import com.skhuthon.caffeinebalance.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        log.info(String.valueOf(oAuth2User));
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = createOAuth2Response(registrationId, oAuth2User.getAttributes());
+
+        if (oAuth2Response == null) {
+            return null; // 처리할 수 없는 등록 ID인 경우 null 반환
+        }
+
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        User existData = userRepository.findByUsername(username);
+
+        if (existData == null) {
+            UserCreateRequestDTO userRequestDto = UserCreateRequestDTO.fromOAuth2Response(oAuth2Response, username);
+            User user = userRequestDto.toEntity();
+            userRepository.save(user);
+
+            UserResponseDTO userResponseDto = UserResponseDTO.fromEntity(user);
+            return new CustomOAuth2User(userResponseDto);
+        }
+
+        updateUserFromOAuth2Response(existData, oAuth2Response);
+
+        UserResponseDTO userResponseDto = UserResponseDTO.fromEntity(existData);
+        return new CustomOAuth2User(userResponseDto);
+    }
+
+    private OAuth2Response createOAuth2Response(String registrationId, Map<String, Object> attributes) {
+        if (registrationId.equals(SocialType.naver.name())) {
+            return new NaverResponse(attributes);
+        } else if (registrationId.equals(SocialType.google.name())) {
+            return new GoogleResponse(attributes);
+        }
+        return null;
+    }
+
+    private void updateUserFromOAuth2Response(User user, OAuth2Response oAuth2Response) {
+        user.update(oAuth2Response.getName(), oAuth2Response.getEmail());
+    }
+}
