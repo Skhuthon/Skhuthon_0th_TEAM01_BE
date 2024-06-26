025f71e67984af9514085c5f05e67503614a7bda
dongkyunKim <kyun010713@gmail.com>
2024-06-27 02:13:08 +0900

    feat: getter 추가

--- src/main/java/com/skhuthon/caffeinebalance/user/dto/response/UserHeightWeightResponseDTO.java
+++ src/main/java/com/skhuthon/caffeinebalance/user/dto/response/UserHeightWeightResponseDTO.java
@@ -3,11 +3,13 @@ package com.skhuthon.caffeinebalance.user.dto.response;
 import com.skhuthon.caffeinebalance.user.domain.User;
 import com.skhuthon.caffeinebalance.user.dto.request.UserHeightWeightRequestDTO;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class UserHeightWeightResponseDTO {
-    private Double height;
-    private Double weight;
+    private double height;
+    private double weight;
 
     public static UserHeightWeightResponseDTO from(User user) {
         return new UserHeightWeightResponseDTO(user.getHeight(), user.getWeight());
