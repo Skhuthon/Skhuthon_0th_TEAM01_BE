301418c5c8d41a5c1531c2f2314df8f97e153c6b
Lee, yeeun <ye7426@naver.com>
2024-06-27 02:27:39 +0900

    feat: MainPageController 구현

---
+++ src/main/java/com/skhuthon/caffeinebalance/mainpage/controller/MainPageController.java
@@ -0,0 +1,30 @@
+package com.skhuthon.caffeinebalance.mainpage.controller;
+
+import com.skhuthon.caffeinebalance.mainpage.dto.request.MainPageRequestDTO;
+import com.skhuthon.caffeinebalance.mainpage.dto.response.MainPageResponseDTO;
+import com.skhuthon.caffeinebalance.mainpage.service.MainPageService;
+import com.skhuthon.caffeinebalance.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/main")
+@RequiredArgsConstructor
+public class MainPageController {
+    private final MainPageService mainPageService;
+
+    @GetMapping
+    public ResponseEntity<MainPageResponseDTO> getMainPageInfo(@AuthenticationPrincipal User user) {
+        MainPageResponseDTO mainPageResponseDTO = mainPageService.getUserCaffeineInfo(user);
+        return new ResponseEntity<>(mainPageResponseDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/addcaffeine")
+    public ResponseEntity<Void> addCaffeineIntake(@AuthenticationPrincipal User user, @RequestBody MainPageRequestDTO requestDTO) {
+        mainPageService.updateTodayCaffeineIntake(user, requestDTO.getTodayCaffeineIntakeAmount());
+        return ResponseEntity.ok().build();
+    }
+}
