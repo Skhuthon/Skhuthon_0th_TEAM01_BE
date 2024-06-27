d928dced5d0f4bf545fc98724dc1c4f6db17ae95
dongkyunKim <kyun010713@gmail.com>
2024-06-28 00:59:08 +0900

    feat: 사용자 정보 조회 메소드 추가

--- src/main/java/com/skhuthon/caffeinebalance/user/service/UserService.java
+++ src/main/java/com/skhuthon/caffeinebalance/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.skhuthon.caffeinebalance.user.service;
 import com.skhuthon.caffeinebalance.user.domain.User;
 import com.skhuthon.caffeinebalance.user.dto.request.UserHeightWeightRequestDTO;
 import com.skhuthon.caffeinebalance.user.dto.response.UserHeightWeightResponseDTO;
+import com.skhuthon.caffeinebalance.user.dto.response.UserResponseDTO;
 import com.skhuthon.caffeinebalance.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -18,15 +19,24 @@ public class UserService {
 
     @Transactional
     public UserHeightWeightResponseDTO updateHeightAndWeight(UserHeightWeightRequestDTO userHeightWeightRequestDTO) {
+        User user = getCurrentUser();
+        user.updateHeightAndWeight(userHeightWeightRequestDTO.height(), userHeightWeightRequestDTO.weight());
+
+        return UserHeightWeightResponseDTO.from(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponseDTO getUserInfo() {
+        User user = getCurrentUser();
+        return UserResponseDTO.of(user);
+    }
+
+    private User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
             throw new IllegalArgumentException("인증되지 않은 사용자입니다.");
         }
-
         String username = authentication.getName();
-        User user = userRepository.findByUsername(username);
-        user.updateHeightAndWeight(userHeightWeightRequestDTO.height(), userHeightWeightRequestDTO.weight());
-
-        return UserHeightWeightResponseDTO.from(user);
+        return userRepository.findByUsername(username);
     }
 }
