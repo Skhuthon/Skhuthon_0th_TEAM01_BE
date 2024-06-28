3888e9eb9c12af47597c4c8ecc85d0da91433094
dongkyunKim <kyun010713@gmail.com>
2024-06-29 02:22:26 +0900

    feat: ProductRequestDTO 클래스 정의
    - 브랜드, 메뉴, 키워드 DTO 클래스 추가
    - 입력 제약 조건에 대한 유효성 검사 어노테이션 포함

---
+++ src/main/java/com/skhuthon/caffeinebalance/caffeine/dto/request/ProductRequestDTO.java
@@ -0,0 +1,37 @@
+package com.skhuthon.caffeinebalance.caffeine.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ProductRequestDTO {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Brand {
+        @Schema(description = "브랜드")
+        @NotNull(message = "브랜드는 null이 될 수 없습니다")
+        private String brand;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Menu {
+        @Schema(description = "메뉴")
+        @NotNull(message = "메뉴는 null이 될 수 없습니다")
+        private String Menu;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class KeyWord {
+        @Schema(description = "검색 키워드")
+        @NotNull(message = "검색 키워드는 null이 될 수 없습니다")
+        private String keyword;
+    }
+}
