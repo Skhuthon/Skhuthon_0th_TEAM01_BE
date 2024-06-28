90fc4d038a584f7a7f60b5aa7851c44c858a2599
dongkyunKim <kyun010713@gmail.com>
2024-06-28 23:22:24 +0900

    feat: 표준화된 오류 코드와 메시지를 위한 ErrorCode 열거형 추가
    - HttpStatus와 메시지에 해당하는 오류 코드를 정의
    - 잘못된 속성, 인증되지 않은 사용자 및 리소스 없음에 대한 오류 코드 포함

---
+++ src/main/java/com/skhuthon/caffeinebalance/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.skhuthon.caffeinebalance.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+    INVALID_ATTRIBUTE_TYPE_MESSAGE(HttpStatus.BAD_REQUEST, "잘못된 응답 속성 유형입니다."),
+
+    /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
+    INVALID_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+
+    /* 404 NOT_FOUND : 리소스를 찾을 수 없음 */
+    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 메뉴입니다."),
+    BRAND_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 브랜드입니다."),
+    BRAND_AND_MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 메뉴브랜드입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
