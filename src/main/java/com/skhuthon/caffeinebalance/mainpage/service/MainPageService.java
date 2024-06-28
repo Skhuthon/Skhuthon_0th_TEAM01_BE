c32adc7bfbfdeba25ea7b0365181b070b0df906d
Lee, yeeun <ye7426@naver.com>
2024-06-28 23:35:35 +0900

    fix: 메인 페이지 서비스 수정

--- src/main/java/com/skhuthon/caffeinebalance/mainpage/service/MainPageService.java
+++ src/main/java/com/skhuthon/caffeinebalance/mainpage/service/MainPageService.java
@@ -1,12 +1,15 @@
 package com.skhuthon.caffeinebalance.mainpage.service;
 
+import com.skhuthon.caffeinebalance.mainpage.domain.MainPageDrink;
+import com.skhuthon.caffeinebalance.mainpage.dto.response.MainPageDrinkResponseDTO;
+import com.skhuthon.caffeinebalance.mainpage.dto.response.MainPageResponseDTO;
 import com.skhuthon.caffeinebalance.mainpage.repository.MainPageCaffeineIntakeRepository;
 import com.skhuthon.caffeinebalance.user.domain.User;
 import com.skhuthon.caffeinebalance.user.repository.UserRepository;
-import com.skhuthon.caffeinebalance.mainpage.dto.response.MainPageResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,21 +17,23 @@ public class MainPageService {
 
     private final MainPageCaffeineIntakeRepository mainPageCaffeineIntakeRepository;
     private final UserRepository userRepository;
+    private final MainPageDrinkService mainPageDrinkService;
 
     public MainPageResponseDTO getUserCaffeineInfo(User user) {
         int recommendedCaffeineIntakeAmount = user.getRecommendedCaffeineIntakeAmount();
         int todayCaffeineIntakeAmount = user.getTodayCaffeineIntakeAmount();
         int canCaffeineIntakeAmount = recommendedCaffeineIntakeAmount - todayCaffeineIntakeAmount;
 
+        List<MainPageDrink> recommendedDrinks = mainPageDrinkService.recommendDrinks(canCaffeineIntakeAmount);
+        MainPageDrinkResponseDTO mainPageDrinkResponseDTO = MainPageDrinkResponseDTO.builder()
+                .recommendedDrinks(recommendedDrinks)
+                .build();
+
         return MainPageResponseDTO.builder()
                 .recommendedCaffeineIntakeAmount(recommendedCaffeineIntakeAmount)
                 .todayCaffeineIntakeAmount(todayCaffeineIntakeAmount)
                 .canCaffeineIntakeAmount(canCaffeineIntakeAmount)
+                .mainPageDrinkResponseDTO(mainPageDrinkResponseDTO)
                 .build();
-    }
-
-    @Transactional
-    public void updateTodayCaffeineIntake(User user, int todayCaffeineIntakeAmount) {
-        user.setTodayCaffeineIntakeAmount(todayCaffeineIntakeAmount);
     }
 }
