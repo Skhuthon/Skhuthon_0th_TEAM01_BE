abfd17c4e882e207bef62982ab0a15933824571a
Lee, yeeun <ye7426@naver.com>
2024-06-29 18:28:08 +0900

    feat: 음료 추천 서비스 구현

--- src/main/java/com/skhuthon/caffeinebalance/Product/service/ProductService.java
+++ src/main/java/com/skhuthon/caffeinebalance/Product/service/ProductService.java
@@ -19,29 +19,29 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepository coffeeRepository;
+    private final ProductRepository productRepository;
     private final UserRepository userRepository;
 
     public ProductResponseDTO.Brands getBrand() {
-        List<String> brands = coffeeRepository.getBrands().orElseThrow(
+        List<String> brands = productRepository.getBrands().orElseThrow(
                 () -> new CustomException(ErrorCode.BRAND_NOT_FOUND));
         return ProductResponseDTO.Brands.of(brands);
     }
 
     public ProductResponseDTO.Menu getMenuByBrand(ProductRequestDTO.Brand brand) {
-        List<String> menu = coffeeRepository.findMenuByBrand(brand.getBrand()).orElseThrow(
+        List<String> menu = productRepository.findMenuByBrand(brand.getBrand()).orElseThrow(
                 () -> new CustomException(ErrorCode.BRAND_NOT_FOUND));
         return ProductResponseDTO.Menu.of(menu);
     }
 
     public ProductResponseDTO.Caffeine getCaffeineByMenu(ProductRequestDTO.Brand brand, ProductRequestDTO.Menu menu) {
-        Double caffeine = coffeeRepository.findCaffeineByMenu(brand.getBrand(), menu.getMenu()).orElseThrow(
+        Double caffeine = productRepository.findCaffeineByMenu(brand.getBrand(), menu.getMenu()).orElseThrow(
                 () -> new CustomException(ErrorCode.BRAND_AND_MENU_NOT_FOUND));
         return ProductResponseDTO.Caffeine.of(caffeine);
     }
 
     public ProductResponseDTO.ProductSearch getMenuSearchList(ProductRequestDTO.KeyWord keyword) {
-        List<Product> caffeineList = coffeeRepository.findByMenuContaining(keyword.getKeyword());
+        List<Product> caffeineList = productRepository.findByMenuContaining(keyword.getKeyword());
         return ProductSearch.from(caffeineList);
     }
 
@@ -52,6 +52,12 @@ public class ProductService {
         user.updateCaffeineInformation(caffeine, canCaffeineIntakeAmount);
 
         return UserCaffeineResponseDTO.of(user);
+    }
+
+    public ProductResponseDTO.RecommendProduct getRecommendProduct() {
+        User user = getCurrentUser();
+        Product product = productRepository.findRandomCaffeineByCanCaffeineIntakeAmount(user.getCanCaffeineIntakeAmount());
+        return ProductResponseDTO.RecommendProduct.from(product);
     }
 
     private User getCurrentUser() {
